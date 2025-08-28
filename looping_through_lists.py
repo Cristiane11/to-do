abd6ab50e2200723a3a6e6f2e73ffdbf6706256e
Cristiane Abreu <bjwmcd@yahoo.com>
2025-08-27 21:35:20 -0400

    update

---
+++ looping_through_lists.py
@@ -0,0 +1,10 @@
+tasks= ["do Homework", "Go to the Gym", " Groceries"]
+#Method 1:
+print("\nMETHOD 1:")
+for task in tasks:
+    print(task)
+
+#Method 2:
+print("\nMETHOD 2:")
+for i  in range(len(tasks)):
+    print(i)
